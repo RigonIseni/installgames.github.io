d0486bb34991296a97e02bfad91a9d5c7407d190
Rigon <96444356+RigonIseni@users.noreply.github.com>
2022-04-07 21:58:06 +0200

    I made some changes!

--- main.js
+++ main.js
@@ -2,18 +2,78 @@ const nav = document.querySelector("nav");
 const toggler = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
 const x = document.querySelector(".iksi");
+const back = document.querySelector(".black");
+const link = document.querySelector(".links");
+const link1 = document.querySelector(".links1");
+const link2 = document.querySelector(".links2");
+const link3 = document.querySelector(".links3");
+
+link.addEventListener("click", () => {
+    if(nav.classList.contains("active")){
+        nav.classList.remove("active");
+        menu.style.display = "block";
+        x.style.display = "none";
+        back.classList.remove("active");
+    }else{
+        nav.classList.add("active");
+        menu.style.display = "none";
+        x.style.display = "block";
+        back.classList.add("active");
+    }
+})
+link1.addEventListener("click", () => {
+    if(nav.classList.contains("active")){
+        nav.classList.remove("active");
+        menu.style.display = "block";
+        x.style.display = "none";
+        back.classList.remove("active");
+    }else{
+        nav.classList.add("active");
+        menu.style.display = "none";
+        x.style.display = "block";
+        back.classList.add("active");
+    }
+})
+link2.addEventListener("click", () => {
+    if(nav.classList.contains("active")){
+        nav.classList.remove("active");
+        menu.style.display = "block";
+        x.style.display = "none";
+        back.classList.remove("active");
+    }else{
+        nav.classList.add("active");
+        menu.style.display = "none";
+        x.style.display = "block";
+        back.classList.add("active");
+    }
+})
+link3.addEventListener("click", () => {
+    if(nav.classList.contains("active")){
+        nav.classList.remove("active");
+        menu.style.display = "block";
+        x.style.display = "none";
+        back.classList.remove("active");
+    }else{
+        nav.classList.add("active");
+        menu.style.display = "none";
+        x.style.display = "block";
+        back.classList.add("active");
+    }
+})
 
 toggler.addEventListener("click", () => {
     if(nav.classList.contains("active")){
         nav.classList.remove("active");
         menu.style.display = "block";
         x.style.display = "none";
+        back.classList.remove("active");
     }else{
         nav.classList.add("active");
         menu.style.display = "none";
         x.style.display = "block";
+        back.classList.add("active");
     }
-})
+});
 
 const loader = document.querySelector("#preloader");
 const body = document.querySelector("body");
@@ -21,4 +81,4 @@ const body = document.querySelector("body");
 window.addEventListener("load", () => {
 loader.style.display = "none";
 body.style.overflowY = "scroll";
-})
+});
